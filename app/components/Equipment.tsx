3276f98f52f7349376a8314264ffed2a55064e43
Julien Usson <usson.julien@gmail.com>
2023-03-05 10:18:58 +0100

    :sparkles:

--- app/components/Equipment.tsx
+++ app/components/Equipment.tsx
@@ -5,7 +5,13 @@ import type { Equipment as EquipmentEntity } from '@prisma/client'
 import blueLayout from '~/assets/blue.jpg'
 import greenLayout from '~/assets/green.jpg'
 import yellowLayout from '~/assets/yellow.jpg'
-import Marked from './Marked'
+import red1Layout from '~/assets/red1.jpg'
+import red2Layout from '~/assets/red2.jpg'
+import red3Layout from '~/assets/red3.jpg'
+import orange1Layout from '~/assets/orange1.jpg'
+import orange2Layout from '~/assets/orange2.jpg'
+import orange3Layout from '~/assets/orange3.jpg'
+import Marked from '~/components/Marked'
 
 interface Props extends Partial<EquipmentEntity> {
   sx?: SxProps<Theme>
@@ -22,7 +28,9 @@ export default function Equipment({ sx, ...equipment }: Props) {
             case 'green':
               return <GreenLayout {...equipment} />
             case 'orange':
+              return <OrangeLayout {...equipment} />
             case 'red':
+              return <RedLayout {...equipment} />
             case 'yellow':
               return <YellowLayout {...equipment} />
           }
@@ -88,4 +96,22 @@ function YellowLayout({ description }: Partial<EquipmentEntity>) {
       )}
     </CardContent>
   )
+}
+
+function RedLayout({ description }: Partial<EquipmentEntity>) {
+  return (
+    <CardContent sx={{ backgroundImage: `url(${red1Layout})` }}>
+      {description && (
+        <Marked
+          sx={{ position: 'absolute', top: '6mm', left: '27mm', right: '6mm', bottom: '4mm' }}
+        >
+          {description}
+        </Marked>
+      )}
+    </CardContent>
+  )
+}
+
+function OrangeLayout({}: Partial<EquipmentEntity>) {
+  return <CardContent sx={{ backgroundImage: `url(${orange1Layout})` }}></CardContent>
 }
