603e3227dd5c2970ead6d8fb9ec6f3e913243624
Julien Usson <usson.julien@gmail.com>
2023-03-05 13:16:50 +0100

    :sparkles:

--- app/components/Ennemy.tsx
+++ app/components/Ennemy.tsx
@@ -1,21 +1,24 @@
 import type { SxProps, Theme } from '@mui/material'
-import { Typography } from '@mui/material'
 import { Stack, styled } from '@mui/material'
 import type { Ennemy as EnnemyEntity } from '@prisma/client'
-import { marked } from 'marked'
+
+import ennemyLayout from '~/assets/ennemy.png'
+import Marked from './Marked'
 
 interface Props extends Partial<EnnemyEntity> {
   sx?: SxProps<Theme>
 }
 
-export default function Ennemy({ sx, ...ennemy }: Props) {
+export default function Ennemy({ sx, description }: Props) {
   return (
     <Stack alignItems="center" justifyContent="center" height="100%" sx={sx}>
       <Card>
         <CardContent direction="row">
-          {ennemy.description && (
-            <Typography dangerouslySetInnerHTML={{ __html: marked(ennemy.description) }} />
-          )}
+          <Description
+            sx={{ position: 'absolute', top: '13mm', left: '6mm', right: '6mm', bottom: '12mm' }}
+          >
+            {description ?? ''}
+          </Description>
         </CardContent>
       </Card>
     </Stack>
@@ -23,15 +26,28 @@ export default function Ennemy({ sx, ...ennemy }: Props) {
 }
 
 const Card = styled('div')({
-  width: '88mm',
-  height: '63mm',
+  height: '88mm',
+  width: '63mm',
   backgroundColor: '#fff',
   padding: '2mm',
+  '& *': {
+    fontFamily: "'Orbitron', sans-serif !important",
+  },
 })
 
 const CardContent = styled(Stack)({
-  backgroundColor: '#eee',
+  position: 'relative',
   borderRadius: '2mm',
   margin: '1mm',
   height: 'calc(100% - 2mm)',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundImage: `url(${ennemyLayout})`,
+})
+const Description = styled(Marked)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#212121',
 })
